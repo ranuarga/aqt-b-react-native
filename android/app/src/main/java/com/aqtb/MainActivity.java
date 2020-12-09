6fff2a668c61631425dd5ecda5c0a41e98e14164
ranuarga <august.arga@gmail.com>
2020-12-09 14:30:14 +0700

    Add: Handle SplashScreen Jumped in Xiaomi

--- android/app/src/main/java/com/aqtb/MainActivity.java
+++ android/app/src/main/java/com/aqtb/MainActivity.java
@@ -2,14 +2,26 @@ package com.aqtb;
 
 import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
+import android.os.Build;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this, R.style.SplashStatusBarTheme);
-      super.onCreate(savedInstanceState);
+    int themeId = R.style.SplashStatusBarTheme;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      switch (Build.MANUFACTURER) {
+        case "Xiaomi":
+          themeId = R.style.SplashStatusBarThemeXiaomi;
+          break;
+        default:
+          break;
+      }
+    }
+
+    SplashScreen.show(this, themeId);
+    super.onCreate(savedInstanceState);
   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
