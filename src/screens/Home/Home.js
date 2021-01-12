f90d5e84381317ae1dd1104048865d519de2e66f
ranuarga <august.arga@gmail.com>
2021-01-12 20:05:15 +0700

    Add: Split All Sensor Data From MQTT

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -10,7 +10,13 @@ export default class Home extends React.Component {
     state = {
         isConnected: false,
         loading: false,
-        message: ''
+        feed1: '',
+        ph: '',
+        temperature: '',
+        do2: '',
+        ammonia: '',
+        feed2: '',
+        duty_cycle: '' 
     }
 
     componentDidMount() {
@@ -35,8 +41,15 @@ export default class Home extends React.Component {
     }
 
     onSub = message => {
+        let split_msg = message.split('#')
         this.setState({
-            message,
+            feed1: split_msg[0],
+            ph: split_msg[1],
+            temperature: split_msg[2],
+            do2: split_msg[3],
+            ammonia: split_msg[4],
+            feed2: split_msg[5],
+            duty_cycle: split_msg[6]
         })
     }
 
@@ -56,7 +69,16 @@ export default class Home extends React.Component {
     }    
 
     render() {
-        const { isConnected, message } = this.state
+        const { 
+            isConnected,
+            feed1,
+            ph,
+            temperature,
+            do2,
+            ammonia,
+            feed2,
+            duty_cycle 
+        } = this.state
         return (
             <React.Fragment>
                 <Header/>
@@ -67,7 +89,25 @@ export default class Home extends React.Component {
                         AQT28X
                     </Text>
                     <Text style={styles.instructions}>
-                        You received message: {message}
+                        pH value: {ph}
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Temperature value: {temperature} &deg;C
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Dissolved Oxygen value: {do2} mg/L
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Ammonia value: {ammonia} ppm
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Duty Cycle value: {duty_cycle} %
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Feeder 1 value: {feed1} cm
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Feeder 2 value: {feed2} cm
                     </Text>
                     <TouchableOpacity onPress={this.onLogout}>
                         <Text>LOGOUT</Text>
